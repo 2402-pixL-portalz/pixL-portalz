77641cf601fa1355aded0d2d160482b508ff6681
Gabriel <segmentedpuma@LAPTOP-DKL1MBK8>
2024-05-15 15:40:23 -0500

    added formatting to exit.js

--- src/game/assets/objects/exit/exit.js
+++ src/game/assets/objects/exit/exit.js
@@ -11,15 +11,15 @@ const exitObject = (level) => {
 }
 
 //creates an exit which takes in the exit object, and array with the x/y positions, and an array with the length/height of the exit 
-const createExit = (exit, [xPosition,yPosition],[lengthScale, heightScale]) => {
+const createExit = (exit, [xPosition, yPosition], [lengthScale, heightScale]) => {
   exit.create(xPosition, yPosition, "exit").setScale(lengthScale, heightScale).refreshBody();
 }
 
 //checks, when the player is overlapping with the exit, to see if the user is pressing down the S or DOWN key, if they are, it starts the scene provided in the parameters
 const goThroughExit = (level, scene) => {
-if(level.controls.S.isDown || level.controls.DOWN.isDown){
-  level.scene.start(scene);
-}
+  if (level.controls.S.isDown || level.controls.DOWN.isDown) {
+    level.scene.start(scene);
+  }
 }
 
-export{exitLoad, exitObject, createExit, goThroughExit}
+export { exitLoad, exitObject, createExit, goThroughExit }
