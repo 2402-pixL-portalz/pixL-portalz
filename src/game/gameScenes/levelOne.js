4baca0b0095c547ab6c60bdede343cf18ef8613f
Gabriel <segmentedpuma@LAPTOP-DKL1MBK8>
2024-05-15 09:21:07 -0500

    added file structure

--- src/game/gameScenes/levelOne.js
+++ src/game/gameScenes/levelOne.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
-import mC from "../assets/images/character/pixilart-drawing.png"
-import verycool from "../assets/images/character/verycool.png"
+import mC from "../game/assets/images/character/pixilart-drawing.png"
+import verycool from "../game/assets/images/character/verycool.png"
 import Platform from "../assets/objects/platforms/platform";
 
 class Test1 extends Phaser.Scene {
