0bdf9e43bb5577f7d361491f4e24793683140ff5
Gabriel <segmentedpuma@LAPTOP-DKL1MBK8>
2024-05-15 13:51:34 -0500

    created notes for platform.js

--- src/game/assets/objects/platforms/platform.js
+++ src/game/assets/objects/platforms/platform.js
@@ -1,42 +1,25 @@
 import platformImg from "../../images/platforms/platform.png";
 
-//very cool class thingy 
-
-// class Platform {
-//   constructor(level){
-//     this.level = level
-//   }
-
-//   preload(){
-//     return this.level.load.image("platform",platformImg);
-//   }
-
-
-// }
-
-
 //takes in "this" from the scene it is called in
 const Platform = (level) => {
   
-  //returns a statement to preload
+  //runs a statement to preload
   level.load.image("platform", platformImg);
+}
+
+//creation of the object for the platform
+const platformObject = (level) => {
+  //returns details of object to be used in a variable
   return level.physics.add.staticGroup();
 }
 
+//create platforms function takes in the platform object, an array with both the x position and the y position, and an array with the length/height scaling
 const createPlatform = (platform,[xPosition,yPosition],[lengthScale, heightScale]) => {
 
   platform.create(xPosition, yPosition, "platform").setScale(lengthScale, heightScale).refreshBody();
-
+  //returns created platform 
   return platform;
-	
-
 }
-
-const platformObject = (level) => {
-
-  return level.physics.add.staticGroup();
-}
-
 
 
 
