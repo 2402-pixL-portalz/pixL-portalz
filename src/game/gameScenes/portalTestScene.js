31d8c6af0b61260a1390097fbe9e3df630f80a11
Tybreen <tyler.breen@icloud.com>
2024-05-20 15:26:11 -0400

    create functional portals

--- src/game/gameScenes/portalTestScene.js
+++ src/game/gameScenes/portalTestScene.js
@@ -3,36 +3,25 @@ import mC from "../assets/images/character/pixilart-drawing.png";
 import playerVars from "../util functions/playerVars";
 import playerControls from "../util functions/playerControls";
 import dayImage from "./../assets/images/backgrounds/day.png";
-import bluePortalImage from "./../assets/images/portals/bluePortalSheet.png";
+import { addTeleportingOverlap, createPortal, portalLoad, portalUpdate, portalVars } from "../assets/objects/portals/portal";
 
 class PortalTest extends Phaser.Scene {
 	constructor() {
 		super("portal test");
 		playerVars(this);
+		portalVars(this);
+		this.c = 0;
 	}
 
 	preload() {
 		this.load.image(`player`, mC);
 		this.load.image(`day`, dayImage);
 
-		this.load.spritesheet(`bluePortalSheet`, bluePortalImage, { frameWidth: 498, frameHeight: 498 });
+		portalLoad(this, `blue`);
 	}
 
 	create() {
 		this.bg = this.add.image(this.sys.game.config.width / 2, this.sys.game.config.height / 2, "day");
-
-		this.portal = this.add.sprite(this.sys.game.config.width / 3, this.sys.game.config.height - 50, `bluePortalSheet`).setScale(0.2, 0.2);
-
-		this.anims.create({
-			key: "bluePortal",
-			frames: this.anims.generateFrameNumbers("bluePortalSheet"),
-			frameRate: 20,
-			repeat: -1
-		});
-
-		this.portal.play(`bluePortal`);
-
-		this.bg.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
 
 		//player
 		this.player = this.physics.add.image(100, 500, `player`).setScale(1.6, 1.6);
@@ -40,10 +29,20 @@ class PortalTest extends Phaser.Scene {
 
 		this.player.setCollideWorldBounds(true);
 		this.controls = this.input.keyboard.addKeys(`W,S,A,D,UP,DOWN,RIGHT,LEFT,SPACE`);
+
+		this.myPortal = createPortal(this, `blue`, this.sys.game.config.width / 4, this.sys.game.config.height - 100, `down`);
+		this.myPortal2 = createPortal(this, `blue`, this.sys.game.config.width / 4, this.sys.game.config.height - 500, `down`);
+
+		addTeleportingOverlap(this, this.myPortal, this.myPortal2, [this.player]);
+		addTeleportingOverlap(this, this.myPortal2, this.myPortal, [this.player]);
+
+		this.bg.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
 	}
 
 	update() {
 		playerControls(this);
+
+		portalUpdate(this);
 	}
 }
 
