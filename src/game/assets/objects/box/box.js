30481b503c156c616caff934075ca6bbcf456988
tframe98 <tylerframe8@gmail.com>
2024-05-22 13:39:46 -0700

    box physics

---
+++ src/game/assets/objects/box/box.js
@@ -0,0 +1,83 @@
+import Phaser from "phaser";
+
+class Box extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'box');
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    // Physics properties for box
+    this.setCollideWorldBounds(true);
+    this.setBounce(.3);
+    this.setDrag(100, 100);
+    this.setAngularDrag(10);
+    this.setMass(10);
+    this.body.setGravityY(300);
+
+    this.setSize(this.width, this.height);
+    this.setOrigin(.5, .5);
+    this.boxFlipped = false;
+    this.landed = false;
+  }
+
+  handlePush(player) {
+    if (!this.boxFlipped) {
+      if (Math.abs(player.y - this.y) < this.height / 2) {
+        this.boxFlipped = true;
+
+        const flipImpulse = 50;
+        let flipDirection = 1;
+
+        if (player.x < this.x) {
+          flipDirection = 1;
+        } else if (player.x > this.x) {
+          flipDirection = -1;
+        }
+
+        this.setAngularVelocity(flipDirection * flipImpulse);
+        this.setVelocityX(flipDirection * flipImpulse);
+
+        
+      }
+    }
+  }
+
+  
+
+  update() {
+    if (!this.body.blocked.down) {
+      this.setAngularVelocity(this.body.velocity.x);
+      this.landed = false;
+    } else {
+      if (!this.landed) {
+        const forwardForce = Math.sign(this.body.velocity.x) * 85;
+        this.setVelocityX(this.body.velocity.x + forwardForce);
+        this.landed = true;
+      }
+
+      // Gradually reduce angular velocity
+      this.setAngularVelocity(this.body.angularVelocity * .91);
+
+      // If the box is moving slowly, make it rotate to the nearest flat side
+      if (Math.abs(this.body.angularVelocity) < .1 && Math.abs(this.body.velocity.x) < 50) {
+        const angle = this.angle % 360;
+       const nearestRightAngle = Math.round(angle / 90) * 90;
+        const angleDifference = nearestRightAngle - angle;
+
+        if (Math.abs(angleDifference) < 100) {
+          this.setAngle(nearestRightAngle);
+          this.setAngularVelocity(1);
+          this.setVelocityX(1);
+        } else {
+          // Apply a small angular velocity to nudge the box towards a flat side
+          this.setAngularVelocity(angleDifference * 700);
+        }
+      }
+
+      // Gradually reduce linear velocity
+      this.setVelocityX(this.body.velocity.x * 0.95);
+    }
+  }
+}
+
+export default Box;
--- src/game/gameScenes/levelTwo.js
+++ src/game/gameScenes/levelTwo.js
@@ -5,7 +5,11 @@ import playerControls from "../util functions/playerControls";
 import { createPlatform, platformObject, platformLoad } from "../assets/objects/platforms/platform";
 import { exitLoad, exitObject, createExit, goThroughExit } from "../assets/objects/exit/exit";
 import levelTwoBg from "../assets/images/backgrounds/level2.jpg";
-import box from "../assets/images/box/box.png";
+import Box from "../assets/objects/box/box";
+import boxImage from "../assets/images/box/box.png";
+
+
+
 
 
 class LevelTwo extends Phaser.Scene {
@@ -17,9 +21,10 @@ class LevelTwo extends Phaser.Scene {
 	preload() {
 		this.load.image(`player`, mC);
     this.load.image("bg", levelTwoBg);
-		this.load.image("box", box);
+		this.load.image("box", boxImage);
+		platformLoad(this);
 		exitLoad(this);
-    platformLoad(this);
+    
 
 	}
 
@@ -40,78 +45,38 @@ class LevelTwo extends Phaser.Scene {
 
 
 		//player
-		this.player = this.physics.add.image(100, 580, `player`).setScale(1.6, 1.6);
+		this.player = this.physics.add.image(400, 250, `player`).setScale(1.6, 1.6);
 		this.player.body.setMaxVelocityX(this.playerMaxRunSpeed);
 
 		this.player.setCollideWorldBounds(true);
 		this.controls = this.input.keyboard.addKeys(`W,S,A,D,UP,DOWN,RIGHT,LEFT,SPACE`);
 
 		//platforms
-	
+		createPlatform(platforms, [400, 300], [2, 0.5]);
 
     //interacts
 		this.physics.add.collider(this.player, platforms);
 		this.physics.add.overlap(this.player, exit, () => { goThroughExit(this, "Level Select") });
 
-		
+	
 
 		//box sprite
-		this.box = this.physics.add.sprite(150, 580, 'box');
-		this.box.setCollideWorldBounds(true);
-		this.box.setImmovable(true);
-		
-		this.box.body.setSize(this.box.width, this.box.height);
-		this.box.setOrigin(0.5, 0.5);
-		
+		this.box = new Box(this, 450, 250);
 
-		//collision between character and box
-		this.physics.add.collider(this.player, 
-		this.box, this.handlePush, null, this);
-		this.boxFlipped = false;
+		//collision between player and box
+		this.physics.add.collider(this.player, this.box, () => {
+			this.box.handlePush(this.player);
+		});
+
+		//collision between box and platforms
+		this.physics.add.collider(this.box, platforms);
+
+		
 	}
-
-	
-
-	handlePush(player, box ) {
-		if (!this.boxFlipped) {
-			if (Math.abs(player.y - box.y) < box.height / 2) {
-				this.boxFlipped = true;
-
-				const flipDistance = this.box.width;
-
-				let targetX = box.x;
-				let targetAngle = box.angle;
-
-				if (player.x < box.x) {
-					targetX += flipDistance;
-					targetAngle += 90;
-				} else if (player.x > box.x) {
-					targetX -= flipDistance;
-					targetAngle -= 90;
-				}
-
-				this.tweens.add({
-					targets: box,
-					angle: targetAngle,
-
-					x: targetX,
-					duration: 650,
-					ease: 'Power2',
-					onComplete: () => {
-						this.boxFlipped = false;
-					}
-				});
-			}
-		}
-		}
-	
-
-	
 
 	update() {
 		playerControls(this);
-
-		
+		this.box.update();
 	}
 }
 
