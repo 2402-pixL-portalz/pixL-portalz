1f06f407e8d191e72808f2fd6576009b07c52756
lucydelash <lucyrudd614@gmail.com>
2024-05-28 15:52:17 -0400

    added p& connected portal color code: import, portalLoad & createPortal funcs

--- src/game/assets/objects/portals/portal.js
+++ src/game/assets/objects/portals/portal.js
@@ -1,10 +1,10 @@
 import applyPortalTrajectory from "../../../util functions/applyPortalTrajectory";
 import bluePortalImage from "./../../images/portals/bluePortalSheet.png";
-import grayPortalImage from "./../../images/portals/grayPortalSpreadsheet.png";
-import greenPortalImage from "./../../images/portals/greenPortalSpreadsheet.png";
-import orangePortalImage from "./../../images/portals/orangePortalSpreadsheet.png";
-import pinkPortalImage from "./../../images/portals/pinkPortalSpreadsheet.png";
-import redPortalImage from "./../../images/portals/redPortalSpreadsheet.png";
+import grayPortalImage from "./../../images/portals/grayPortalSheet.png";
+import greenPortalImage from "./../../images/portals/greenPortalSheet.png";
+import orangePortalImage from "./../../images/portals/orangePortalSheet.png";
+import pinkPortalImage from "./../../images/portals/pinkPortalSheet.png";
+import redPortalImage from "./../../images/portals/redPortalSheet.png";
 
 const portalVars = (This) => {
 	This.inPortal = false;
@@ -15,6 +15,16 @@ const portalVars = (This) => {
 const portalLoad = (This, color) => {
 	if (color === `blue`) {
 		This.load.spritesheet(`bluePortalSheet`, bluePortalImage, { frameWidth: 498, frameHeight: 498 });
+	} else if (color === `gray`) {
+		This.load.spritesheet(`grayPortalSheet`, grayPortalImage, { frameWidth: 498, frameHeight: 498 });
+	} else if (color === `green`) {
+		This.load.spritesheet(`greenPortalSheet`, greenPortalImage, { frameWidth: 498, frameHeight: 498 });
+	} else if (color === `orange`) {
+		This.load.spritesheet(`orangePortalSheet`, orangePortalImage, { frameWidth: 498, frameHeight: 498 });
+	} else if (color === `pink`) {
+		This.load.spritesheet(`pinkPortalSheet`, pinkPortalImage, { frameWidth: 498, frameHeight: 498 });
+	} else if (color === `red`) {
+		This.load.spritesheet(`redPortalSheet`, redPortalImage, { frameWidth: 498, frameHeight: 498 });
 	} else {
 		throw `We do not support this portal color!`;
 	}
@@ -25,6 +35,16 @@ const createPortal = (This, color, x, y, rotation) => {
 	let sheet;
 	if (color === `blue`) {
 		sheet = `bluePortalSheet`;
+	} else if (color === `gray`) {
+		sheet = `grayPortalSheet`;
+	} else if (color === `green`) {
+		sheet = `greenPortalSheet`;
+	} else if (color === `orange`) {
+		sheet = `orangePortalSheet`;
+	} else if (color === `pink`) {
+		sheet = `pinkPortalSheet`;
+	} else if (color === `red`) {
+		sheet = `redPortalSheet`;
 	}
 
 	const portal = This.physics.add.sprite(x, y, sheet).setScale(0.2, 0.2);
@@ -58,213 +78,6 @@ const createPortal = (This, color, x, y, rotation) => {
 
 	return portal;
 };
-
-//GRAY PORTAL
-const createPortal = (This, color, x, y, rotation) => {
-	let sheet;
-	if (color === `gray`) {
-		sheet = `grayPortalSpreadsheet`;
-	}
-
-	const portal = This.physics.add.sprite(x, y, sheet).setScale(0.2, 0.2);
-
-	portal.body.allowGravity = false;
-
-	if (!Object.hasOwn(This.anims.anims.entries, color)) {
-		This.anims.create({
-			key: color,
-			frames: This.anims.generateFrameNumbers(sheet),
-			frameRate: 20,
-			repeat: -1
-		});
-	}
-
-	portal.play(color);
-
-	if (rotation === `left`) {
-		portal.angle = 0;
-		portal.setSize(275, 475);
-	} else if (rotation === `right`) {
-		portal.angle = 180;
-		portal.setSize(275, 475);
-	} else if (rotation === `down`) {
-		portal.angle = 270;
-		portal.setSize(475, 275);
-	} else if (rotation === `up`) {
-		portal.angle = 90;
-		portal.setSize(475, 275);
-	}
-
-	return portal;
-};
-
-//GREEN PORTAL
-const createPortal = (This, color, x, y, rotation) => {
-	let sheet;
-	if (color === `green`) {
-		sheet = `greenPortalSpreadsheet`;
-	}
-
-	const portal = This.physics.add.sprite(x, y, sheet).setScale(0.2, 0.2);
-
-	portal.body.allowGravity = false;
-
-	if (!Object.hasOwn(This.anims.anims.entries, color)) {
-		This.anims.create({
-			key: color,
-			frames: This.anims.generateFrameNumbers(sheet),
-			frameRate: 20,
-			repeat: -1
-		});
-	}
-
-	portal.play(color);
-
-	if (rotation === `left`) {
-		portal.angle = 0;
-		portal.setSize(275, 475);
-	} else if (rotation === `right`) {
-		portal.angle = 180;
-		portal.setSize(275, 475);
-	} else if (rotation === `down`) {
-		portal.angle = 270;
-		portal.setSize(475, 275);
-	} else if (rotation === `up`) {
-		portal.angle = 90;
-		portal.setSize(475, 275);
-	}
-
-	return portal;
-};
-
-//ORANGE PORTAL
-const createPortal = (This, color, x, y, rotation) => {
-	let sheet;
-	if (color === `orange`) {
-		sheet = `orangePortalSpreadsheet`;
-	}
-
-	const portal = This.physics.add.sprite(x, y, sheet).setScale(0.2, 0.2);
-
-	portal.body.allowGravity = false;
-
-	if (!Object.hasOwn(This.anims.anims.entries, color)) {
-		This.anims.create({
-			key: color,
-			frames: This.anims.generateFrameNumbers(sheet),
-			frameRate: 20,
-			repeat: -1
-		});
-	}
-
-	portal.play(color);
-
-	if (rotation === `left`) {
-		portal.angle = 0;
-		portal.setSize(275, 475);
-	} else if (rotation === `right`) {
-		portal.angle = 180;
-		portal.setSize(275, 475);
-	} else if (rotation === `down`) {
-		portal.angle = 270;
-		portal.setSize(475, 275);
-	} else if (rotation === `up`) {
-		portal.angle = 90;
-		portal.setSize(475, 275);
-	}
-
-	return portal;
-};
-
-//PINK LADY
-const createPortal = (This, color, x, y, rotation) => {
-	let sheet;
-	if (color === `pink`) {
-		sheet = `pinkPortalSpreadsheet`;
-	}
-
-	const portal = This.physics.add.sprite(x, y, sheet).setScale(0.2, 0.2);
-
-	portal.body.allowGravity = false;
-
-	if (!Object.hasOwn(This.anims.anims.entries, color)) {
-		This.anims.create({
-			key: color,
-			frames: This.anims.generateFrameNumbers(sheet),
-			frameRate: 20,
-			repeat: -1
-		});
-	}
-
-	portal.play(color);
-
-	if (rotation === `left`) {
-		portal.angle = 0;
-		portal.setSize(275, 475);
-	} else if (rotation === `right`) {
-		portal.angle = 180;
-		portal.setSize(275, 475);
-	} else if (rotation === `down`) {
-		portal.angle = 270;
-		portal.setSize(475, 275);
-	} else if (rotation === `up`) {
-		portal.angle = 90;
-		portal.setSize(475, 275);
-	}
-
-	return portal;
-};
-
-//RED PORTAL
-const createPortal = (This, color, x, y, rotation) => {
-	let sheet;
-	if (color === `red`) {
-		sheet = `redPortalSpreadsheet`;
-	}
-
-	const portal = This.physics.add.sprite(x, y, sheet).setScale(0.2, 0.2);
-
-	portal.body.allowGravity = false;
-
-	if (!Object.hasOwn(This.anims.anims.entries, color)) {
-		This.anims.create({
-			key: color,
-			frames: This.anims.generateFrameNumbers(sheet),
-			frameRate: 20,
-			repeat: -1
-		});
-	}
-
-	portal.play(color);
-
-	if (rotation === `left`) {
-		portal.angle = 0;
-		portal.setSize(275, 475);
-	} else if (rotation === `right`) {
-		portal.angle = 180;
-		portal.setSize(275, 475);
-	} else if (rotation === `down`) {
-		portal.angle = 270;
-		portal.setSize(475, 275);
-	} else if (rotation === `up`) {
-		portal.angle = 90;
-		portal.setSize(475, 275);
-	}
-
-	return portal;
-};
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const joinPortals = (This, enteringPortal, exitingPortal, objectsAllowedToOverlap) => {
