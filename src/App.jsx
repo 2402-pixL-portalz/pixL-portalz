d02bf900c0afaff4f917c883262d16989dda5d01
Tyler Breen <tyler.breen@icloud.com>
2024-06-03 11:18:51 -0400

    Revert "added logout button (when logged in)"

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import Game from './components/Game';
-import Navbar from './components/Navbar';
-import GamePage from './pages/GamePage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import Game from "./components/Game";
+import Navbar from "./components/Navbar";
+import GamePage from "./pages/GamePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import { Route, Routes } from "react-router-dom";
+
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
-  const game = new Game();
+	const game = new Game();
 
-  useEffect(() => {
-    // Check if the user is already logged in
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsLoggedIn(true);
-    }
-  }, []);
 
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
-    navigate('/login');
-  };
+	return (
+		<>
+			<Navbar game={game} />
 
-  return (
-    <>
-      <Navbar game={game} isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+			<Routes>
+				<Route path="/Login" element={<Login />} />
+				<Route path="/Register" element={<Register />} />
+				<Route path="/" element={<GamePage game={game}/>} />
+			</Routes>
+		</>
+	);
 
-      <Routes>
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<GamePage game={game} />} />
-      </Routes>
-    </>
-  );
+	
 }
 
 export default App;
+
+
